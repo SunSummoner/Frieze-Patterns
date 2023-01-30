4fccb36f46c38799181162df80031f6e51b36652
Kritika Verma <95202116+SunSummoner@users.noreply.github.com>
2023-01-30 22:28:33 +0530

    Added image path

--- img.py
+++ img.py
@@ -1,5 +1,5 @@
 from PIL import Image
 from numpy import array
-im_1 = Image.open(r"C:\Users\DELL\Desktop\CIC\Semester 4\Talks\IMG-20230120-WA0012.jpg")
+im_1 = Image.open(r"image path")
 a = array(im_1)
 print(a)
